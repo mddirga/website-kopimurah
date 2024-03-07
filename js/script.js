b5f32f6da7c1d7e698443b2ba0f8ea7c2a075b71
madedirga22 <madedirga22@gmail.com>
2024-03-07 21:29:02 +0800

    Sidebar Menu

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// Toggle Class Active untuk hamburger menu
+const navbarNav = document.querySelector(".navbar-nav");
+
+// Jika Hamburger Menu diKlik
+document.querySelector("#hamburger-menu").onclick = () => {
+    navbarNav.classList.toggle("active");
+  };
+
+// Klik di Luar Element
+const hm = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e){
+    if (!hm.contains(e.target) && !navbarNav.contains(e.target)) {
+        navbarNav.classList.remove("active");
+      }
+});
